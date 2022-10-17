d73efecefc10248a14abd9c62a2b27a8256767a4
Max <88346491+bannman71@users.noreply.github.com>
2022-10-17 14:27:27 +0000

    making more pages

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 
 var server = app.listen(3000);
 
 app.use(express.static('public'));
+
+
+router.get('/Home', function(req, res, next) {
+  res.render('users', { title: 'Users' });
+});
 
 console.log('listening on port 3000');
