b7a379636c31ff38cdff7ef06b9901e064fb0e29
Max <88346491+bannman71@users.noreply.github.com>
2022-10-18 09:41:04 +0000

    routers are implemented

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
+const express = require('express');
+const app = express();
+const router = express.Router();
+
 
 var server = app.listen(3000);
 
-app.use(express.static('public'));
-
-
-router.get('/Home', function(req, res, next) {
-  res.render('users', { title: 'Users' });
+router.get('/Home', (req,res) => {
+  res.send('Hello World, This is home router');
 });
 
+
+router.get('/Puzzles', (req,res) => {
+  res.send('Hello World, This is puzzles router');
+});
+
+
+
+router.get('/BoardEditor', (req,res) => {
+  res.send('Hello World, This is board editor router');
+});
+
+
+router.get('/PlayAi', (req,res) => {
+  res.send('Hello World, This is AI router');
+});
+
+app.use(express.static(`${__dirname}/public`));
+app.use('/', router);
+
+
+app.listen(4000);
 console.log('listening on port 3000');
