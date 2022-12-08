7651956ab9096ee61dd12bb5dc0383d82e3dc64c
Max <maxs29@icloud.com>
2022-12-08 14:25:21 +0000

    Game options box update

--- public/boardEditor.js
+++ public/boardEditor.js
@@ -132,16 +132,7 @@ new p5(function(p5){
         }
 
 
-        endOfFen = '';
-        if ($('#white-to-move').is(':checked')) endOfFen += ' w '; else endOfFen += ' b ';
-        if ($('#white-castling-short').is(':checked')) endOfFen += 'K';
-        if ($('#white-castling-long').is(':checked')) endOfFen += 'Q';
-        if ($('#black-castling-short').is(':checked')) endOfFen += 'k';
-        if ($('#black-castling-long').is(':checked')) endOfFen += 'q';
-
-        $('#FEN-container').html(boardFEN + endOfFen + ' - 0 1');
-
-
+     
     }
 
     setInterval(() => {
@@ -236,7 +227,20 @@ new p5(function(p5){
 
         if (board.isOnBoard(getClickedSquare.y,getClickedSquare.x)){
             board.occSquares[getClickedSquare.y][getClickedSquare.x] = new Piece((selectedPiece & 7), getClickedSquare.y,getClickedSquare.x, (selectedPiece & 24));
-        }
+        }   
+
+
+        //this here now do this please pretty
+        endOfFen = '';
+        if ($('#white-to-move').is(':checked')) endOfFen += ' w '; else endOfFen += ' b ';
+        if ($('#white-castling-short').is(':checked')) endOfFen += 'K';
+        if ($('#white-castling-long').is(':checked')) endOfFen += 'Q';
+        if ($('#black-castling-short').is(':checked')) endOfFen += 'k';
+        if ($('#black-castling-long').is(':checked')) endOfFen += 'q';
+
+        $('#FEN-container').html(boardFEN + endOfFen + ' - 0 1');//ASDHJEAGSHJDGAHJSDGHAJSD
+
+
 
     }
 
